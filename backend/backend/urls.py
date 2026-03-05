95e9e1c0cb7668e1ccba2a7eecea9612dab6c07c
shashank-shekhar-tripathi <tripathiji931@gmail.com>
2026-03-05 20:05:22 +0530

    test

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,7 +3,13 @@ from django.urls import path, include
 from rest_framework_simplejwt.views import TokenRefreshView
 from todos.views import RegisterView, LoginView
 
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Taskify Backend Running 🚀")
+
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/auth/register/', RegisterView.as_view(), name='register'),
     path('api/auth/login/', LoginView.as_view(), name='login'),
