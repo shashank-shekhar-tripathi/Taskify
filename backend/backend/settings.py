c2101dc77543f0b52f90236ab51ae35f7c0d6772
shashank-shekhar-tripathi <tripathiji931@gmail.com>
2026-03-05 21:10:58 +0530

    Fix CORS for production

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -102,9 +102,14 @@ SIMPLE_JWT = {
     'ROTATE_REFRESH_TOKENS': True,
 }
 
-CORS_ALLOWED_ORIGINS = config(
-    'CORS_ALLOWED_ORIGINS',
-    default='http://localhost:3000,http://127.0.0.1:3000'
-).split(',')
+# CORS_ALLOWED_ORIGINS = config(
+#     'CORS_ALLOWED_ORIGINS',
+#     default='http://localhost:3000,http://127.0.0.1:3000'
+# ).split(',')
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_TRUSTED_ORIGINS = [
+    "https://taskify-sigma-peach.vercel.app"
+]
 
 CORS_ALLOW_CREDENTIALS = True
